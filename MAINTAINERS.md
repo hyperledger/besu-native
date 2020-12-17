cc752a7a424d116086a16b7656016068ebf92475
Danno Ferrin <danno.ferrin@gmail.com>
2020-12-16 20:44:57 -0700

    Common Repository Structure compliance (#25)
    Add files missing to match common repository structure. Changelog,
    code of conduct, contributing, maintainers, security, some tests.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

---
+++ MAINTAINERS.md
@@ -0,0 +1,4 @@
+# Maintainers
+
+Maintainers for this repository are the same as
+for [Hyperledger Besu](https://github.com/hyperledger/besu/blob/master/MAINTAINERS.md)
---
+++ bls12-381/src/test/java/org/hyperledger/besu/nativelib/bls12_381/BLS12G2MultiExpPrecompiledContractTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package org.hyperledger.besu.nativelib.bls12_381;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import com.google.common.io.CharStreams;
+import com.sun.jna.ptr.IntByReference;
+import org.apache.tuweni.bytes.Bytes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class BLS12G2MultiExpPrecompiledContractTest {
+
+  @Parameterized.Parameter(0)
+  public String input;
+  @Parameterized.Parameter(1)
+  public String expectedResult;
+  @Parameterized.Parameter(2)
+  public String expectedGasUsed;
+  @Parameterized.Parameter(3)
+  public String notes;
+
+  @Parameterized.Parameters
+  public static Iterable<String[]> parameters() throws IOException {
+    return CharStreams.readLines(
+            new InputStreamReader(
+                BLS12G2MultiExpPrecompiledContractTest.class.getResourceAsStream("g2_multiexp.csv"),
+                UTF_8))
+        .stream()
+        .map(line -> line.split(",", 4))
+        .collect(Collectors.toList());
+  }
+
+  @Test
+  public void shouldCalculate() {
+    if ("input".equals(input)) {
+      // skip the header row
+      return;
+    }
+    final byte[] input = Bytes.fromHexString(this.input).toArrayUnsafe();
+
+    final byte[] output = new byte[LibEthPairings.EIP2537_PREALLOCATE_FOR_RESULT_BYTES];
+    final IntByReference outputLength = new IntByReference();
+    final byte[] error = new byte[LibEthPairings.EIP2537_PREALLOCATE_FOR_ERROR_BYTES];
+    final IntByReference errorLength = new IntByReference();
+
+    LibEthPairings.eip2537_perform_operation(
+        LibEthPairings.BLS12_G2MULTIEXP_OPERATION_RAW_VALUE,
+        input,
+        input.length,
+        output,
+        outputLength,
+        error,
+        errorLength);
+
+    final Bytes expectedComputation =
+        expectedResult == null ? null : Bytes.fromHexString(expectedResult);
+    if (errorLength.getValue() > 0) {
+      assertThat(new String(error, 0, errorLength.getValue(), UTF_8)).isEqualTo(notes);
+      assertThat(outputLength.getValue()).isZero();
+    } else {
+      final Bytes actualComputation = Bytes.wrap(output, 0, outputLength.getValue());
+      assertThat(actualComputation).isEqualTo(expectedComputation);
+    }
+  }
+}
