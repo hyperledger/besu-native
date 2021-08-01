1c6b2b3836a4e0b1df17a795a114b7064f958d7f
Daniel Lehrner <daniel@io.builders>
2021-08-02 01:23:14 +0200

    specify architecture for openssl build to avoid error in pipeline
    Signed-off-by: Daniel Lehrner <daniel@io.builders>

--- build.sh
+++ build.sh
@@ -162,12 +162,37 @@ EOF
   # delete old build dir, if exists
   rm -rf "$SCRIPTDIR/secp256r1/build" || true
 
-  ./setup.sh && ./build.sh
+  if [[ "$OSTYPE" == "msys" ]]; then
+  	LIBRARY_EXTENSION=dll
+  	TARGET=Cygwin-x86_64
+  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    LIBRARY_EXTENSION=so
+    TARGET=linux-x86_64
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    LIBRARY_EXTENSION=dylib
+    TARGET=darwin64-x86_64
+  fi
+
+  git submodule init
+  git submodule update
+
+  cd openssl
+  ./Configure enable-ec_nistp_64_gcc_128 no-stdio no-ocsp no-nextprotoneg no-module \
+              no-legacy no-gost no-engine no-dynamic-engine no-deprecated no-comp \
+              no-cmp no-capieng no-ui-console no-tls no-ssl no-dtls no-aria no-bf \
+              no-blake2 no-camellia no-cast no-chacha no-cmac no-des no-dh no-dsa \
+              no-ecdh no-idea no-md4 no-mdc2 no-ocb no-poly1305 no-rc2 no-rc4 no-rmd160 \
+              no-scrypt no-seed no-siphash no-siv no-sm2 no-sm3 no-sm4 no-whirlpool $TARGET
+  make build_generated libcrypto.$LIBRARY_EXTENSION
+
+  cd ../
+
+   ./build.sh
 }
 
-build_secp256k1
-build_altbn128
-build_bls12_381
+#build_secp256k1
+#build_altbn128
+#build_bls12_381
 build_secp256r1
-build_jars
+#build_jars
 exit
