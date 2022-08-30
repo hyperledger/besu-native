498f5bca7972cc9e7e39b62acc3265d879e63c21
Antoine Toulme <antoine@lunar-ocean.com>
2022-08-30 08:22:30 -0700

    Add safe submodule directory (#76)
    * Add safe submodule directory
    
    Signed-off-by: Antoine Toulme <antoine@lunar-ocean.com>
    
    * select the mac runner
    
    Signed-off-by: Antoine Toulme <antoine@lunar-ocean.com>
    
    * Builder selection
    
    Add safe directory
    
    Name CI job
    
    Signed-off-by: Antoine Toulme <antoine@lunar-ocean.com>
    Signed-off-by: Ry Jones <ry@linux.com>
    Co-authored-by: Ry Jones <ry@linux.com>

--- native-build.sh
+++ native-build.sh
@@ -4,4 +4,6 @@ DEBIAN_FRONTEND=non-interactive apt-get install -y autoconf build-essential libt
 export CARGO_HOME="$HOME/.cargo"
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.62.1
 export PATH=$PATH:$CARGO_HOME/bin
+git config --global --add safe.directory /home/ubuntu/secp256r1/besu-native-ec
+git config --global --add safe.directory /home/ubuntu/secp256r1/besu-native-ec/openssl
 /home/ubuntu/build.sh
