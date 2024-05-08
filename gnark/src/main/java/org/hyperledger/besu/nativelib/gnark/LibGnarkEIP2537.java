78b813b0155c1d2fccff6ea2e5db6c66d6e8b993
garyschulte <garyschulte@gmail.com>
2024-05-07 22:25:10 -0700

    working g1Add poc, with test
    Signed-off-by: garyschulte <garyschulte@gmail.com>

---
+++ gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP2537.java
@@ -0,0 +1,45 @@
+package org.hyperledger.besu.nativelib.gnark;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+
+import java.nio.ByteBuffer;
+
+public class LibGnarkEIP2537 implements Library {
+
+    @SuppressWarnings("WeakerAccess")
+    public static final boolean ENABLED;
+
+    static {
+      boolean enabled;
+      try {
+        Native.register(LibGnarkEIP2537.class, "gnark_eip_2537");
+        enabled = true;
+      } catch (final Throwable t) {
+        t.printStackTrace();
+        enabled = false;
+      }
+      ENABLED = enabled;
+    }
+
+    public static final int EIP2537_PREALLOCATE_FOR_ERROR_BYTES = 256;
+
+    public static final int EIP2537_PREALLOCATE_FOR_RESULT_BYTES = 256;
+
+//    public static final byte BLS12_G1ADD_OPERATION_RAW_VALUE = 1;
+//    public static final byte BLS12_G1MUL_OPERATION_RAW_VALUE = 2;
+//    public static final byte BLS12_G1MULTIEXP_OPERATION_RAW_VALUE = 3;
+//    public static final byte BLS12_G2ADD_OPERATION_RAW_VALUE = 4;
+//    public static final byte BLS12_G2MUL_OPERATION_RAW_VALUE = 5;
+//    public static final byte BLS12_G2MULTIEXP_OPERATION_RAW_VALUE = 6;
+//    public static final byte BLS12_PAIR_OPERATION_RAW_VALUE = 7;
+//    public static final byte BLS12_MAP_FP_TO_G1_OPERATION_RAW_VALUE = 8;
+//    public static final byte BLS12_MAP_FP2_TO_G2_OPERATION_RAW_VALUE = 9;
+
+    public static native int eip2537blsG1Add(
+        ByteBuffer input,
+        ByteBuffer output,
+        int inputSize, int outputSize);
+
+}
