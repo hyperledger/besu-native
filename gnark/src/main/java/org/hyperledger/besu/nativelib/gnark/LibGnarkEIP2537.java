38a8c1a18375af464f2b0cf127e7e4714d227068
garyschulte <garyschulte@gmail.com>
2024-05-11 02:51:35 -0700

    attempt at DRY
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP2537.java
+++ gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP2537.java
@@ -20,21 +20,7 @@ public class LibGnarkEIP2537 implements Library {
       ENABLED = enabled;
     }
 
-    public static final int EIP2537_PREALLOCATE_FOR_ERROR_BYTES = 256;
-
   public static final int EIP2537_PREALLOCATE_FOR_RESULT_BYTES = 256;
-
-//    public static final byte BLS12_G1ADD_OPERATION_RAW_VALUE = 1;
-//    public static final byte BLS12_G1MUL_OPERATION_RAW_VALUE = 2;
-//    public static final byte BLS12_G1MULTIEXP_OPERATION_RAW_VALUE = 3;
-
-//    public static final byte BLS12_G2ADD_OPERATION_RAW_VALUE = 4;
-//    public static final byte BLS12_G2MUL_OPERATION_RAW_VALUE = 5;
-//    public static final byte BLS12_G2MULTIEXP_OPERATION_RAW_VALUE = 6;
-
-//    public static final byte BLS12_PAIR_OPERATION_RAW_VALUE = 7;
-//    public static final byte BLS12_MAP_FP_TO_G1_OPERATION_RAW_VALUE = 8;
-//    public static final byte BLS12_MAP_FP2_TO_G2_OPERATION_RAW_VALUE = 9;
 
   public static native int eip2537blsG1Add(
       byte[] input,
