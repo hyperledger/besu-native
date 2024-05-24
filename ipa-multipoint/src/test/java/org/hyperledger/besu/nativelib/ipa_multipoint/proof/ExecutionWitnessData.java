658e3ce582370bf47995691893d01d60b27b366a
Karim Taam <karim.t2am@gmail.com>
2024-05-24 12:25:10 +0200

    clean code
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/proof/ExecutionWitnessData.java
+++ ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/proof/ExecutionWitnessData.java
@@ -6,73 +6,45 @@ import java.util.List;
 
 public class ExecutionWitnessData {
 
-        public Header header;
-
-        public List<String> transactions;
-
-        public List<String> withdrawals;
-
-        @JsonProperty("executionWitness")
-
-        public ExecutionWitness executionWitness;
+    @JsonProperty("header")
+    public Header header;
+    @JsonProperty("transactions")
+    public List<String> transactions;
+    @JsonProperty("withdrawals")
+    public List<String> withdrawals;
+    @JsonProperty("executionWitness")
+    public ExecutionWitness executionWitness;
 
 
     static class Header {
 
         @JsonProperty("blockNumber")
-
         public String blockNumber;
-
         @JsonProperty("parentHash")
-
         public String parentHash;
-
         @JsonProperty("coinbase")
-
         public String coinbase;
-
         @JsonProperty("stateRoot")
-
         public String stateRoot;
-
         @JsonProperty("receiptsRoot")
-
         public String receiptsRoot;
-
         @JsonProperty("logsBloom")
-
         public String logsBloom;
-
         @JsonProperty("gasLimit")
-
         public String gasLimit;
-
         @JsonProperty("gasUsed")
-
         public String gasUsed;
-
         @JsonProperty("timestamp")
-
         public String timestamp;
-
         @JsonProperty("extraData")
-
         public String extraData;
-
         @JsonProperty("baseFeePerGas")
-
         public String baseFeePerGas;
-
         @JsonProperty("blockHash")
-
         public String blockHash;
-
         @JsonProperty("prevRandao")
-
         public String prevRandao;
-
         @JsonProperty("transactionsTrie")
-
         public String transactionsTrie;
 
     }
@@ -81,11 +53,8 @@ public class ExecutionWitnessData {
     static class ExecutionWitness {
 
         @JsonProperty("stateDiff")
-
         public List<StateDiff> stateDiff;
-
         @JsonProperty("verkleProof")
-
         public VerkleProof verkleProof;
 
     }
@@ -93,10 +62,9 @@ public class ExecutionWitnessData {
 
     static class StateDiff {
 
+        @JsonProperty("stem")
         public String stem;
-
         @JsonProperty("suffixDiffs")
-
         public List<SuffixDiff> suffixDiffs;
 
     }
@@ -104,14 +72,11 @@ public class ExecutionWitnessData {
 
     static class SuffixDiff {
 
+        @JsonProperty("suffix")
         public int suffix;
-
         @JsonProperty("currentValue")
-
         public String currentValue;
-
         @JsonProperty("newValue")
-
         public String newValue;
 
     }
@@ -120,21 +85,15 @@ public class ExecutionWitnessData {
     static class VerkleProof {
 
         @JsonProperty("otherStems")
-
         public List<String> otherStems;
-
         @JsonProperty("depthExtensionPresent")
-
         public String depthExtensionPresent;
-
         @JsonProperty("commitmentsByPath")
-
         public List<String> commitmentsByPath;
 
+        @JsonProperty("d")
         public String d;
-
         @JsonProperty("ipaProof")
-
         public IpaProof ipaProof;
 
     }
@@ -142,12 +101,11 @@ public class ExecutionWitnessData {
 
     static class IpaProof {
 
+        @JsonProperty("cl")
         public List<String> cl;
-
+        @JsonProperty("cr")
         public List<String> cr;
-
         @JsonProperty("finalEvaluation")
-
         public String finalEvaluation;
 
     }
