997ff5c6b3fbc056bdcbbf3289e0fdfb830160e5
Karim Taam <karim.t2am@gmail.com>
2024-06-03 10:47:53 +0200

    clean code
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/proof/ExecutionWitnessData.java
+++ ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/proof/ExecutionWitnessData.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright Besu Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package org.hyperledger.besu.nativelib.ipa_multipoint.proof;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
@@ -10,10 +24,6 @@ public class ExecutionWitnessData {
 
     @JsonProperty("header")
     public Header header;
-    @JsonProperty("transactions")
-    public List<String> transactions;
-    @JsonProperty("withdrawals")
-    public List<String> withdrawals;
     @JsonProperty("executionWitness")
     public ExecutionWitness executionWitness;
 
@@ -24,30 +34,8 @@ public class ExecutionWitnessData {
         public String blockNumber;
         @JsonProperty("parentHash")
         public String parentHash;
-        @JsonProperty("coinbase")
-        public String coinbase;
         @JsonProperty("stateRoot")
         public String stateRoot;
-        @JsonProperty("receiptsRoot")
-        public String receiptsRoot;
-        @JsonProperty("logsBloom")
-        public String logsBloom;
-        @JsonProperty("gasLimit")
-        public String gasLimit;
-        @JsonProperty("gasUsed")
-        public String gasUsed;
-        @JsonProperty("timestamp")
-        public String timestamp;
-        @JsonProperty("extraData")
-        public String extraData;
-        @JsonProperty("baseFeePerGas")
-        public String baseFeePerGas;
-        @JsonProperty("blockHash")
-        public String blockHash;
-        @JsonProperty("prevRandao")
-        public String prevRandao;
-        @JsonProperty("transactionsTrie")
-        public String transactionsTrie;
 
     }
 
--- ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/proof/VerifyProofTest.java
+++ ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/proof/VerifyProofTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright Besu Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package org.hyperledger.besu.nativelib.ipa_multipoint.proof;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -5,12 +20,15 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.nativelib.ipamultipoint.LibIpaMultipoint;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,49 +40,25 @@ public class VerifyProofTest {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
-    @Test
-    public void TestVerifyPreStateRootValidFor1() throws IOException {
-        final InputStream inputStream = VerifyProofTest.class.getResourceAsStream("/valid_block_1.json");
-        final ExecutionWitnessData executionWitnessData = objectMapper.readValue(inputStream, new TypeReference<>() {
-        });
-        final Bytes prestateRoot = Bytes.fromHexString("0x1fbf85345a3cbba9a6d44f991b721e55620a22397c2a93ee8d5011136ac300ee");
-        assertThat(verifyPreState(executionWitnessData, prestateRoot)).isTrue();
+
+    public static Stream<Arguments> getParameters() {
+        return Stream.of(
+                Arguments.of("/valid_block_1.json","0x1fbf85345a3cbba9a6d44f991b721e55620a22397c2a93ee8d5011136ac300ee", true),
+                Arguments.of("/valid_block_72.json", "0x64e1a647f42e5c2e3c434531ccf529e1b3e93363a40db9fc8eec81f492123510", true),
+                Arguments.of("/invalid_block_72.json", "0x64e1a647f42e5c2e3c434531ccf529e1b3e93363a40db9fc8eec81f492123510", false),
+                Arguments.of("/valid_block_73.json", "0x18d1dfcc6ccc6f34d14af48a865895bf34bde7f3571d9ba24a4b98122841048c", true),
+                Arguments.of("/invalid_block_73.json", "0x18d1dfcc6ccc6f34d14af48a865895bf34bde7f3571d9ba24a4b98122841048c", false)
+                );
     }
 
-    @Test
-    public void TestVerifyPreStateRootValidFor72() throws IOException {
-        final InputStream inputStream = VerifyProofTest.class.getResourceAsStream("/valid_block_72.json");
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("getParameters")
+    public void TestVerifyPreStateRoot(final String fileName, final String preStateRoot, final boolean isValid) throws IOException {
+        final InputStream inputStream = VerifyProofTest.class.getResourceAsStream(fileName);
         final ExecutionWitnessData executionWitnessData = objectMapper.readValue(inputStream, new TypeReference<>() {
         });
-        final Bytes prestateRoot = Bytes.fromHexString("0x64e1a647f42e5c2e3c434531ccf529e1b3e93363a40db9fc8eec81f492123510");
-        assertThat(verifyPreState(executionWitnessData, prestateRoot)).isTrue();
-    }
-
-    @Test
-    public void TestVerifyPreStateRootNotValidFor72() throws IOException {
-        final InputStream inputStream = VerifyProofTest.class.getResourceAsStream("/invalid_block_72.json");
-        final ExecutionWitnessData executionWitnessData = objectMapper.readValue(inputStream, new TypeReference<>() {
-        });
-        final Bytes prestateRoot = Bytes.fromHexString("0x64e1a647f42e5c2e3c434531ccf529e1b3e93363a40db9fc8eec81f492123510");
-        assertThat(verifyPreState(executionWitnessData, prestateRoot)).isFalse();
-    }
-
-    @Test
-    public void TestVerifyPreStateRootValidFor73() throws IOException {
-        final InputStream inputStream = VerifyProofTest.class.getResourceAsStream("/valid_block_73.json");
-        final ExecutionWitnessData executionWitnessData = objectMapper.readValue(inputStream, new TypeReference<>() {
-        });
-        final Bytes prestateRoot = Bytes.fromHexString("0x18d1dfcc6ccc6f34d14af48a865895bf34bde7f3571d9ba24a4b98122841048c");
-        assertThat(verifyPreState(executionWitnessData, prestateRoot)).isTrue();
-    }
-
-    @Test
-    public void TestVerifyPreStateRootNotValidFor73() throws IOException {
-        final InputStream inputStream = VerifyProofTest.class.getResourceAsStream("/invalid_block_73.json");
-        final ExecutionWitnessData executionWitnessData = objectMapper.readValue(inputStream, new TypeReference<>() {
-        });
-        final Bytes prestateRoot = Bytes.fromHexString("0x18d1dfcc6ccc6f34d14af48a865895bf34bde7f3571d9ba24a4b98122841048c");
-        assertThat(verifyPreState(executionWitnessData, prestateRoot)).isFalse();
+        final Bytes prestateRoot = Bytes.fromHexString(preStateRoot);
+        assertThat(verifyPreState(executionWitnessData, prestateRoot)).isEqualTo(isValid);
     }
 
     private boolean verifyPreState(final ExecutionWitnessData executionWitnessData, final Bytes preStateRoot){
