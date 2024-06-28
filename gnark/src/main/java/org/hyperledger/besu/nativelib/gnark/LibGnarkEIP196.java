45961281baa5a098340ec207cae9c29ed4946b31
garyschulte <garyschulte@gmail.com>
2024-06-28 11:53:39 -0700

    remove duplicate findFirstTrailingZeroIndex function
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP196.java
+++ gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP196.java
@@ -68,16 +68,6 @@ public class LibGnarkEIP196 {
     return ret;
   }
 
-  public int findFirstTrailingZeroIndex(byte[] array) {
-    for (int i = array.length - 1; i >= 0; i--) {
-      if (array[i] != 0) {
-        return i + 1; // The first trailing zero is after this non-zero byte
-      }
-    }
-    // If all bytes are zero, return 0
-    return 0;
-  }
-
   public static native int eip196altbn128G1Add(
       byte[] input,
       byte[] output,
