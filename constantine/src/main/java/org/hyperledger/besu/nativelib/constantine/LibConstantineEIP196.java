079cfd331076c32475da15251664e722433e56f1
Nischal Sharma <nischal@web3labs.com>
2024-07-12 13:08:55 +0530

    C bindings and jni used
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

--- constantine/src/main/java/org/hyperledger/besu/nativelib/constantine/LibConstantineEIP196.java
+++ constantine/src/main/java/org/hyperledger/besu/nativelib/constantine/LibConstantineEIP196.java
@@ -1,20 +1,38 @@
 package org.hyperledger.besu.nativelib.constantine;
 
-import com.sun.jna.Native;
-
 public class LibConstantineEIP196 {
-
-    public static final int EIP196_PREALLOCATE_FOR_RESULT_BYTES = 128;
-    public static final int EIP196_PREALLOCATE_FOR_ERROR_BYTES = 256;
-
     static {
-        Native.register(LibConstantineEIP196.class, "constantine_eip196");
+        System.loadLibrary("constantine");
     }
 
-    public static native int eth_evm_bn254_g1add(byte[] output, int outputLen, byte[] input, int inputLen);
+    public native int ctt_eth_evm_bn254_g1add(byte[] r, int r_len, byte[] inputs, int inputs_len);
+    public native int ctt_eth_evm_bn254_g1mul(byte[] r, int r_len, byte[] inputs, int inputs_len);
+    public native int ctt_eth_evm_bn254_pairingCheck(byte[] r, int r_len, byte[] inputs, int inputs_len);
 
-    public static native int eth_evm_bn254_g1mul(byte[] output, int outputLen, byte[] input, int inputLen);
+    public static void main(String[] args) {
+        LibConstantineEIP196 constInstance = new LibConstantineEIP196();
 
-    public static native int eth_evm_bn254_ecpairingcheck(byte[] output, int outputLen, byte[] input, int inputLen);
+        byte[] r = new byte[64];
+        byte[] inputs = new byte[128];
+        int status = constInstance.ctt_eth_evm_bn254_g1add(r, r.length, inputs, inputs.length);
+        System.out.println("ctt_eth_evm_bn254_g1add status: " + status + ", result: " + bytesToHex(r));
 
+        r = new byte[64];
+        inputs = new byte[96];
+        status = constInstance.ctt_eth_evm_bn254_g1mul(r, r.length, inputs, inputs.length);
+        System.out.println("ctt_eth_evm_bn254_g1mul status: " + status + ", result: " + bytesToHex(r));
+
+        r = new byte[32];
+        inputs = new byte[256];
+        status = constInstance.ctt_eth_evm_bn254_pairingCheck(r, r.length, inputs, inputs.length);
+        System.out.println("ctt_eth_evm_bn254_pairingCheck status: " + status + ", result: " + bytesToHex(r));
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
