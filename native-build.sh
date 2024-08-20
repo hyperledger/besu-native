c7034eaa13a5deb4d1838d18b6d782949caff81b
Nischal Sharma <nischal@web3labs.com>
2024-08-21 02:58:26 +0530

    java 11 removed and java21 apt install added
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

--- native-build.sh
+++ native-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-DEBIAN_FRONTEND=non-interactive apt-get install -y autoconf build-essential libtool automake patchelf curl openjdk-11-jre-headless git wget
+DEBIAN_FRONTEND=non-interactive apt-get install -y autoconf build-essential libtool automake patchelf curl openjdk-21-jdk git wget
 wget https://go.dev/dl/go1.20.2.linux-arm64.tar.gz
 echo "78d632915bb75e9a6356a47a42625fd1a785c83a64a643fedd8f61e31b1b3bef  go1.20.2.linux-arm64.tar.gz" | sha256sum -c || exit 1
 tar -xzf go1.20.2.linux-arm64.tar.gz -C $HOME
@@ -13,8 +13,4 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.75.0
 export PATH=$PATH:$CARGO_HOME/bin
 git config --global --add safe.directory /home/ubuntu/secp256r1/besu-native-ec
 git config --global --add safe.directory /home/ubuntu/secp256r1/besu-native-ec/openssl
-wget https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-aarch64_bin.tar.gz
-tar -xzf openjdk-21.0.2_linux-aarch64_bin.tar.gz -C $HOME
-export JAVA_HOME=$HOME/jdk-21.0.2
-export PATH=$JAVA_HOME/bin:$PATH
 /home/ubuntu/build.sh
