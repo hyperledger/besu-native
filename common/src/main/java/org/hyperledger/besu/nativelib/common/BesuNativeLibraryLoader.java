f24c5319d2f55a7d09195b16dc216304a7d4e3c3
garyschulte <garyschulte@gmail.com>
2025-03-05 16:37:26 -0800

    add new common module
    Signed-off-by: garyschulte <garyschulte@gmail.com>

---
+++ common/src/main/java/org/hyperledger/besu/nativelib/common/BesuNativeLibraryLoader.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright Besu Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package org.hyperledger.besu.nativelib.common;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+public class BesuNativeLibraryLoader {
+
+  // base path for lib/arch.  replace underscore with dash to conform to jar packaging
+  static final String LIBRARY_RESOURCE_ARCH_PATH =
+      "lib/" + System.getProperty("os.arch").replace("_", "-") + "/";
+
+  /**
+   * Wraps JNA with a prescriptive path, removing any platform naming inconsistencies
+   *  that might exist.  E.g. linux-gnu-x86-64 vs linux-x86_64.
+   *
+   *  Note that this method expects the directory space to be "partitioned" only by the arch, and
+   *  relies on the different operating systems to have different shared library object
+   *  filenames.  E.g. .dylib vs .so vs .dll.
+   */
+  public static void registerJNA(Class jniClass, String libraryName) {
+
+    try {
+      final Optional<Path> libPath = extract(jniClass, libraryName);
+
+      if (libPath.isPresent()) {
+        NativeLibrary lib = NativeLibrary.getInstance(libPath.get().toString());
+        Native.register(jniClass, lib);
+      } else {
+        throw new UnsatisfiedLinkError();
+      }
+    } catch (UnsatisfiedLinkError __) {
+        String exceptionMessage =
+            String.format(
+                "Couldn't load native library (%s). It wasn't available at %s or the library path.",
+                libraryName, asLibraryResourcePath(libraryName));
+        throw new RuntimeException(exceptionMessage);
+    }
+  }
+
+  public static void loadJNI(Class jniClass, String libraryName) {
+
+    try {
+      final Optional<Path> libPath = extract(jniClass, libraryName);
+
+      if (libPath.isPresent()) {
+        System.load(libPath.get().toString());
+      } else {
+        System.loadLibrary(libraryName);
+      }
+    } catch (UnsatisfiedLinkError __) {
+      String exceptionMessage =
+          String.format(
+              "Couldn't load native library (%s). It wasn't available at %s or the library path.",
+              libraryName, asLibraryResourcePath(libraryName));
+      throw new RuntimeException(exceptionMessage);
+    }
+  }
+
+
+  private static Optional<Path> extract(Class jniClass, String libraryName) {
+    final String platformNativeLibraryName = System.mapLibraryName(libraryName);
+
+    // load from lib/arch.  replace underscore with dash to avoid platform arch naming oddities
+    final String libraryResourcePath = asLibraryResourcePath(libraryName);
+
+    InputStream libraryResource = jniClass.getResourceAsStream(libraryResourcePath);
+
+    if (libraryResource == null) {
+      // try absolute classpath reference for filesystem resources in case we are running tests:
+      libraryResource = jniClass.getResourceAsStream("/" + libraryResourcePath);
+    }
+
+    if (libraryResource != null) {
+      try {
+        Path tempDir = Files.createTempDirectory(libraryName + "@");
+        tempDir.toFile().deleteOnExit();
+        Path tempDll = tempDir.resolve(platformNativeLibraryName);
+        tempDll.toFile().deleteOnExit();
+        Files.copy(libraryResource, tempDll, StandardCopyOption.REPLACE_EXISTING);
+        libraryResource.close();
+        return Optional.of(tempDll);
+      } catch (IOException ex) {
+        throw new UncheckedIOException(ex);
+      }
+    }
+    return Optional.empty();
+  }
+
+  private static String asLibraryResourcePath(String libraryName) {
+
+    final String platformNativeLibraryName = System.mapLibraryName(libraryName);
+    return LIBRARY_RESOURCE_ARCH_PATH + platformNativeLibraryName;
+
+  }
+}
