c5afeb32a646360e8cce93a11f4a6fe85e3c497f
garyschulte <garyschulte@gmail.com>
2025-03-17 06:48:03 -0700

    release 1.3.0 (#257)
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- gnark/gnark-jni/gnark-eip-196.go
+++ gnark/gnark-jni/gnark-eip-196.go
@@ -6,10 +6,11 @@ package main
 import "C"
 import (
 	"errors"
-	"github.com/consensys/gnark-crypto/ecc/bn254"
 	"math/big"
 	"reflect"
 	"unsafe"
+
+	"github.com/consensys/gnark-crypto/ecc/bn254"
 )
 
 var ErrMalformedPointEIP196 = errors.New("invalid point encoding")
