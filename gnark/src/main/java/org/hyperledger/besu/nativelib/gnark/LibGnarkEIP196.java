2542efae57547fbb81ade4bf296305d0281975c7
Simon Dudley <simon.dudley@consensys.net>
2025-10-29 17:18:28 +1000

    Optimize EIP-196 AltBn128 EcAdd
    1. Memory Pooling for Performance
    
    Introduces sync.Pool objects to reuse allocations and reduce garbage collection pressure:
    - bigIntPool - reuses big.Int allocations for scalar operations
    - g1Pool / g2Pool - reuses elliptic curve point allocations
    - bytes64Pool - reuses 64-byte buffer allocations
    
    2. Simplified Error Handling
    
    - Before: Functions returned error strings passed through buffers between Go and Java
    - After: Functions return integer error codes (0 for success, 1-8 for various errors)
    - Removes overhead of string allocation and copying across JNI boundary
    
    3. Streamlined JNI Interface
    
    Changes function signatures from:
    func eip196altbn128G1Add(input, output, errorBuf *C.char, inputLen C.int,
                             outputLen, errorLen *C.int) C.int
    To:
    func eip196altbn128G1Add(input, output *C.char, inputLen C.int) errorCode
    
    4. Optimized Field Validation
    
    - Removes manual field checking (checkInFieldEIP196 function)
    - Uses SetBytesCanonical() which performs validation internally
    - Eliminates redundant modulus comparisons
    
    5. Direct Encoding
    
    - g1AffineEncode now works directly with point objects using RawBytes()
    - Eliminates intermediate Marshal() allocations
    
    6. Reduced Buffer Sizes
    
    - Result buffer size reduced from 128 to 64 bytes (only needs to hold one G1 point)
    - Removes 256-byte error buffer entirely
    
    Signed-off-by: Simon Dudley <simon.dudley@consensys.net>
    Co-authored-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP196.java
+++ gnark/src/main/java/org/hyperledger/besu/nativelib/gnark/LibGnarkEIP196.java
@@ -20,14 +20,32 @@ import org.hyperledger.besu.nativelib.common.BesuNativeLibraryLoader;
 
 public class LibGnarkEIP196 {
 
-  public static final int EIP196_PREALLOCATE_FOR_RESULT_BYTES = 128;
-  public static final int EIP196_PREALLOCATE_FOR_ERROR_BYTES = 256; // includes error string
+  public static final int EIP196_PREALLOCATE_FOR_RESULT_BYTES = 64;
   @SuppressWarnings("WeakerAccess")
   public static final byte EIP196_ADD_OPERATION_RAW_VALUE = 1;
   public static final byte EIP196_MUL_OPERATION_RAW_VALUE = 2;
   public static final byte EIP196_PAIR_OPERATION_RAW_VALUE = 3;
 
   public static final boolean ENABLED;
+
+  // errCodeSuccess errorCode = iota
+	// errCodeMalformedPointEIP196
+	// errCodeInvalidInputPairingLengthEIP196
+	// errCodePointNotInFieldEIP196
+	// errCodePointInSubgroupCheckFailedEIP196
+	// errCodePointOnCurveCheckFailedEIP196
+	// errCodePairingCheckErrorEIP196
+	// errCodePairingResultFailed // hack - it is not an error, but we use it to indicate that the pairing check result is 0
+  public static final int EIP196_ERR_CODE_SUCCESS = 0;
+  public static final int EIP196_ERR_CODE_MALFORMED_POINT = 1;
+  public static final int EIP196_ERR_CODE_INVALID_INPUT_PAIRING_LENGTH = 2;
+  public static final int EIP196_ERR_CODE_POINT_NOT_IN_FIELD = 3;
+  public static final int EIP196_ERR_CODE_POINT_IN_SUBGROUP_CHECK_FAILED = 4;
+  public static final int EIP196_ERR_CODE_POINT_ON_CURVE_CHECK_FAILED = 5;
+  public static final int EIP196_ERR_CODE_PAIRING_CHECK_ERROR = 6;
+  public static final int EIP196_ERR_CODE_PAIRING_RESULT_FAILED = 7;
+  public static final int EIP196_ERR_CODE_WRITE_BUFFER_FAILED = 8;
+
 
   static {
     boolean enabled;
@@ -48,21 +66,32 @@ public class LibGnarkEIP196 {
       byte op,
       byte[] i,
       int i_len,
-      byte[] output,
-      IntByReference o_len,
-      byte[] err,
-      IntByReference err_len) {
+      byte[] output) {
 
     int ret = -1;
     switch(op) {
       case EIP196_ADD_OPERATION_RAW_VALUE:
-        ret = eip196altbn128G1Add(i, output, err, i_len, o_len, err_len);
+        ret = eip196altbn128G1Add(i, output, i_len);
         break;
       case  EIP196_MUL_OPERATION_RAW_VALUE:
-        ret = eip196altbn128G1Mul(i, output, err, i_len, o_len, err_len);
+        ret = eip196altbn128G1Mul(i, output, i_len);
         break;
       case EIP196_PAIR_OPERATION_RAW_VALUE:
-        ret = eip196altbn128Pairing(i, output, err, i_len, o_len, err_len);
+        ret = eip196altbn128Pairing(i, output, i_len);
+        switch (ret) {
+          case EIP196_ERR_CODE_SUCCESS:
+            // pairing check succeeded. We set the result to 1
+            output[31] = 0x01;
+            break;
+          case EIP196_ERR_CODE_PAIRING_RESULT_FAILED:
+            // pairing check failed. We leave the output intact, which is 0's.
+            // we also override the error code to indicate that the call to the method itself was successful.
+            ret = EIP196_ERR_CODE_SUCCESS;
+            break;
+          default:
+            // some error occurred, we return the error code
+            return ret;
+        }
         break;
       default:
         throw new RuntimeException("Not Implemented EIP-196 operation " + op);
@@ -74,18 +103,15 @@ public class LibGnarkEIP196 {
   public static native int eip196altbn128G1Add(
       byte[] input,
       byte[] output,
-      byte[] error,
-      int inputSize, IntByReference outputSize, IntByReference err_len);
+      int inputSize);
 
   public static native int eip196altbn128G1Mul(
       byte[] input,
       byte[] output,
-      byte[] error,
-      int inputSize, IntByReference output_len, IntByReference err_len);
+      int inputSize);
 
   public static native int eip196altbn128Pairing(
       byte[] input,
       byte[] output,
-      byte[] error,
-      int inputSize, IntByReference output_len, IntByReference err_len);
+      int inputSize);
 }
